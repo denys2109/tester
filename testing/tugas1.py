2a2f9577a4f072a4522f14d1a01e80d02cc576bd
deny sulistiyo <66523924+denys2109@users.noreply.github.com>
2020-10-02 08:24:47 +0700

    Add files via upload

---
+++ testing/tugas1.py
@@ -0,0 +1,35 @@
+#Fungsi Luas Segitiga
+
+def luas_segitiga(alas, tinggi):
+    l_segitiga = 0.5 * alas * tinggi
+    return l_segitiga
+
+#Fungsi Keliling Segitiga
+
+def keliling_segitiga(sisi1, sisi2, sisi3):
+    k_segitiga = sisi1 + sisi2 + sisi3
+    return k_segitiga
+
+#Fungsi Luas Lingkaran
+
+def luas_lingkaran(jari_jari_luas):
+    l_lingkaran = 3.14 * jari_jari_luas * jari_jari_luas
+    return l_lingkaran
+
+#Fungsi Keliling Lingkaran
+
+def keliling_lingkaran(jari_jari_keliling):
+    k_lingkaran = 2 * 3.14 * jari_jari_keliling
+    return k_lingkaran
+
+#Fungsi Luas Persegi
+
+def luas_persegi(sisi):
+    l_persegi = sisi * sisi
+    return l_persegi
+
+#Fungsi Keliling Persegi
+
+def keliling_persegi(sisi):
+    k_persegi = 4 * sisi
+    return k_persegi
